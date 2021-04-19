5da0e4b5be8187872532ae1ee4b778e8d50b8cc7
Robert Wilson <robert.wilson@minex360.com>
2021-04-19 08:05:26 +0000

    Initial commit

---
+++ src/test/java/io/labforward/api/TakeHomeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package io.labforward.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeHomeTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
